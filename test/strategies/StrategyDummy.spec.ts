fb2e53cb398cfbcb76860906969617d081a9fb40
Michael Zhou <zhoumotongxue008@gmail.com>
2021-03-03 19:15:52 +0800

    Add test for RollupChain.commitBlock()
    Cleanup unnecessary Truffle files.

--- test/strategies/StrategyDummy.spec.ts
+++ test/strategies/StrategyDummy.spec.ts
@@ -1,17 +1,26 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
+import { StrategyDummy__factory } from '../../typechain/factories/StrategyDummy__factory';
 import { StrategyDummy } from '../../typechain/StrategyDummy';
 
+declare module 'mocha' {
+  export interface Context {
+    strategyDummy: StrategyDummy;
+  }
+}
+
 describe('StrategyDummy', function () {
-  beforeEach(async function () {});
+  beforeEach(async function () {
+    const strategyDummyFactory = (await ethers.getContractFactory(
+      'StrategyDummy'
+    )) as StrategyDummy__factory;
+    this.strategyDummy = (await strategyDummyFactory.deploy()) as StrategyDummy;
+    await this.strategyDummy.deployed();
+  });
 
   it('should return 1 ether', async function () {
-    const StrategyDummy = await ethers.getContractFactory('StrategyDummy');
-    const strategyDummy = (await StrategyDummy.deploy()) as StrategyDummy;
-
-    await strategyDummy.deployed();
     const oneEther = ethers.utils.parseEther('1');
-    expect(await strategyDummy.syncBalance()).to.equal(oneEther);
+    expect(await this.strategyDummy.syncBalance()).to.equal(oneEther);
   });
 });
