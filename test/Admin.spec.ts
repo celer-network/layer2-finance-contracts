ee62e29786387491970d5b68ceb9a8202434578e
Michael Zhou <zhoumotongxue008@gmail.com>
2021-03-09 15:08:22 +0800

    Add test for withdraw() (#22)

--- test/Admin.spec.ts
+++ test/Admin.spec.ts
@@ -1,6 +1,4 @@
-
 import { expect } from 'chai';
-import { ethers } from 'hardhat';
 
 import { deployContracts } from './common';
 
@@ -16,26 +14,35 @@ describe('Admin', function () {
   it('should fail to deposit when paused', async function () {
     const tokenAddress = this.testERC20.address;
     await this.rollupChain.pause();
-    await expect(this.rollupChain.deposit(tokenAddress, 1)).to.be.revertedWith('Pausable: paused');
+    await expect(this.rollupChain.deposit(tokenAddress, 1)).to.be.revertedWith(
+      'Pausable: paused'
+    );
   });
 
   it('should fail to deposit when exceed limit', async function () {
     const tokenAddress = this.testERC20.address;
-    await this.rollupChain.setNetDepositLimit(tokenAddress, 1)
-    await expect(this.rollupChain.deposit(tokenAddress, 5)).to.be.revertedWith('net deposit exceeds limit');
+    await this.rollupChain.setNetDepositLimit(tokenAddress, 1);
+    await expect(this.rollupChain.deposit(tokenAddress, 5)).to.be.revertedWith(
+      'net deposit exceeds limit'
+    );
   });
 
   it('should fail to drain token when not paused', async function () {
     const tokenAddress = this.testERC20.address;
     await this.rollupChain.deposit(tokenAddress, 10);
-    await expect(this.rollupChain.drainToken(tokenAddress, 10, this.adminSigner.address)).to.be.revertedWith('Pausable: not paused');
+    await expect(
+      this.rollupChain.drainToken(tokenAddress, 10, this.adminSigner.address)
+    ).to.be.revertedWith('Pausable: not paused');
   });
 
   it('should drainToken successfully when paused', async function () {
     const tokenAddress = this.testERC20.address;
     await this.rollupChain.deposit(tokenAddress, 10);
     await this.rollupChain.pause();
-    await this.rollupChain.drainToken(tokenAddress, 10, this.adminSigner.address);
+    await this.rollupChain.drainToken(
+      tokenAddress,
+      10,
+      this.adminSigner.address
+    );
   });
-
 });
