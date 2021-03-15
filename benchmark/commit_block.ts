ae66b0bc0381f71a53b997599fc7fcc8ce54a475
Michael Zhou <zhoumotongxue008@gmail.com>
2021-03-16 00:51:28 +0800

    Minor tweak of benchmark code (#46)

--- benchmark/commit_block.ts
+++ benchmark/commit_block.ts
@@ -16,7 +16,7 @@ describe('Benchmark commitBlock', async function () {
     fs.mkdirSync(GAS_USAGE_DIR, { recursive: true });
   }
   fs.rmSync(GAS_USAGE_LOG, { force: true });
-  fs.appendFileSync(GAS_USAGE_LOG,'transitions, gas cost per block\n\n');
+  fs.appendFileSync(GAS_USAGE_LOG, 'transitions, gas cost per block\n\n');
 
   async function fixture([admin]: Wallet[]) {
     const {
@@ -41,25 +41,37 @@ describe('Benchmark commitBlock', async function () {
   }
 
   async function doBenchmark(txType: string, data: string, maxNum: number) {
-    it('one rollup block with up to ' + maxNum + ' ' + txType + ' transitions', async function () {
-      fs.appendFileSync(GAS_USAGE_LOG, '-- ' + txType + ' --\n');
-      for (let numTxs = 1; numTxs <= maxNum; numTxs++) {
-        if (numTxs > 10 && numTxs%10 != 0) {
-            continue;
+    it(
+      'one rollup block with up to ' + maxNum + ' ' + txType + ' transitions',
+      async function () {
+        this.timeout(20000 + 100 * maxNum);
+
+        fs.appendFileSync(GAS_USAGE_LOG, '-- ' + txType + ' --\n');
+        for (
+          let numTxs = 1;
+          numTxs <= maxNum;
+          numTxs += numTxs >= 10 ? 10 : 1
+        ) {
+          const { rollupChain } = await loadFixture(fixture);
+          let txs = [];
+          for (let i = 0; i < numTxs; i++) {
+            txs.push(data);
+          }
+          const gasUsed = (
+            await (
+              await rollupChain.commitBlock(0, txs, {
+                gasLimit: 9500000 // TODO: Remove once estimateGas() works correctly
+              })
+            ).wait()
+          ).gasUsed;
+          fs.appendFileSync(
+            GAS_USAGE_LOG,
+            numTxs.toString() + '\t' + gasUsed + '\n'
+          );
         }
-        const { rollupChain } = await loadFixture(fixture);
-        let txs = [];
-        for (let i = 0; i < numTxs; i++) {
-          txs.push(data);
-        }
-        const gasUsed = (await (await rollupChain.commitBlock(0, txs)).wait()).gasUsed;
-        fs.appendFileSync(
-          GAS_USAGE_LOG,
-          numTxs.toString() + '\t' + gasUsed + '\n'
-        );
+        fs.appendFileSync(GAS_USAGE_LOG, '\n');
       }
-       fs.appendFileSync(GAS_USAGE_LOG, '\n');
-    });
+    );
   }
 
   await doBenchmark(
@@ -71,7 +83,7 @@ describe('Benchmark commitBlock', async function () {
   await doBenchmark(
     'commit',
     '0x000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000737461746520726f6f740000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000102030000000000000000000000000000000000000000000000000000000000bc614e00000000000000000000000000000000000000000000000000000000000000e0000000000000000000000000000000000000000000000000000000000000005f313238013934696600383629677c3f6a0e467d0439a600383707394901383180113901383889063887a5003933339f203f71540501266d4a8b06380f02380039376c4a0938277f1d00393065299a703400393375003830350e361b577d8c0000',
-    100
+    200
   );
 
   await doBenchmark(
@@ -79,5 +91,4 @@ describe('Benchmark commitBlock', async function () {
     '0x000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000737461746520726f6f7400000000000000000000000000000000000000000000000000000000000012340000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000102030000000000000000000000000000000000000000000000000000000000bc614e0000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000005f313238013934696600383629677c3f6a0e467d0439a600383707394901383180113901383889063887a5003933339f203f71540501266d4a8b06380f02380039376c4a0938277f1d00393065299a703400393375003830350e361b577d8c0000',
     10
   );
-
 });
