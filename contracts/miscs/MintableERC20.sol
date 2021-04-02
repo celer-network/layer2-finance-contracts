c02a4417104d4e8224f7f71383352d55dd17158f
Michael Zhou <zhoumotongxue008@gmail.com>
2021-04-03 05:58:09 +0800

    Add Faucet and MintableERC20 (#78)
    Also include deployment setup.

---
+++ contracts/miscs/MintableERC20.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+
+/**
+ * @title A mintable {ERC20} token.
+ */
+contract MintableERC20 is ERC20Burnable, Ownable {
+    /**
+     * @dev Constructor that gives msg.sender an initial supply of tokens.
+     */
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    /**
+     * @dev Creates `amount` new tokens for `to`.
+     */
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
