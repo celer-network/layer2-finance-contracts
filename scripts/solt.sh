9d4bae1784b84bc46c041222558c3d4e6ea58093
Michael Zhou <zhoumotongxue008@gmail.com>
2021-04-04 06:26:37 +0800

    Add script and instructions for solt (#80)

---
+++ scripts/solt.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Script to run solt and generate standard-json files for Etherscan verification.
+
+solFiles=(
+  Registry
+  TransitionDisputer
+  TransitionEvaluator
+  RollupChain
+  # strategies
+  strategies/interfaces/IStrategy
+  strategies/StrategyDummy
+)
+
+run_solt_write() {
+  for f in ${solFiles[@]}; do
+    solt write contracts/$f.sol --npm --runs 200
+  done
+}
