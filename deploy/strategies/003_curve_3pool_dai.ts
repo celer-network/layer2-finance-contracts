f20c0bba64f4d7f3f2383cdbcc9e627d255fd913
Michael Zhou <zhoumotongxue008@gmail.com>
2021-04-13 17:26:45 +0800

    Add test for StrategyCurve3PoolDAI
    Fix syncBalance()

---
+++ deploy/strategies/003_curve_3pool_dai.ts
@@ -0,0 +1,33 @@
+import * as dotenv from 'dotenv';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+dotenv.config();
+
+const strategyContractName = 'StrategyCurve3PoolDAI';
+const strategyDeploymentName = 'StrategyCurve3PoolDAI';
+
+const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy(strategyContractName, {
+    from: deployer,
+    log: true,
+    args: [
+      process.env.ROLLUP_CHAIN,
+      process.env.CURVE_DAI,
+      process.env.CURVE_3POOL,
+      process.env.CURVE_3CRV,
+      process.env.CURVE_3POOL_GAUGE,
+      process.env.CURVE_3POOL_MINTR,
+      process.env.CURVE_CRV,
+      process.env.WETH,
+      process.env.UNISWAP_ROUTER
+    ]
+  });
+};
+
+deployFunc.tags = [strategyDeploymentName];
+export default deployFunc;
--- test-strategy/README.md
+++ test-strategy/README.md
@@ -1,4 +1,4 @@
-### Test strategies manually
+## Test strategies manually
 
 1. Obtain the test ETH and tokens.
 
@@ -9,3 +9,15 @@
 ```sh
 hardhat test <path-to-test> --network <network>
 ```
+
+## Mainnet forking
+
+1. Make sure `IMPERSONATED_ACCOUNT` is filled out in the env.
+
+2. Run:
+
+```sh
+hardhat node --fork <mainnet-eth-rpc> --fork-block-number <recent-mainchain-block-number>
+```
+
+3. Run the test command with the `localhost` network.
