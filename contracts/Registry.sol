88607cd604f65979383df8210122ecf5df02c704
Michael Zhou <zhoumotongxue008@gmail.com>
2021-04-21 15:52:52 +0800

    Add updateStrategy to Registry

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -17,12 +17,14 @@ contract Registry is Ownable {
 
     event AssetRegistered(address asset, uint32 assetId);
     event StrategyRegistered(address strategy, uint32 strategyId);
+    event StrategyUpdated(address previousStrategy, address newStrategy, uint32 strategyId);
 
     /**
      * @notice Register a asset
      * @param _asset The asset token address;
      */
     function registerAsset(address _asset) external onlyOwner {
+        require(_asset != address(0), "Invalid asset");
         require(assetAddressToIndex[_asset] == 0, "Asset already registered");
 
         // Register asset with an index >= 1 (zero is reserved).
@@ -38,6 +40,7 @@ contract Registry is Ownable {
      * @param _strategy The strategy contract address;
      */
     function registerStrategy(address _strategy) external onlyOwner {
+        require(_strategy != address(0), "Invalid strategy");
         require(strategyAddressToIndex[_strategy] == 0, "Strategy already registered");
 
         // Register strategy with an index >= 1 (zero is reserved).
@@ -46,5 +49,22 @@ contract Registry is Ownable {
         strategyIndexToAddress[numStrategies] = _strategy;
 
         emit StrategyRegistered(_strategy, numStrategies);
+    }
+
+    /**
+     * @notice Update the address of an existing strategy
+     * @param _strategy The strategy contract address;
+     * @param _strategyId The strategy ID;
+     */
+    function updateStrategy(address _strategy, uint32 _strategyId) external onlyOwner {
+        require(_strategy != address(0), "Invalid strategy");
+        require(strategyIndexToAddress[_strategyId] != address(0), "Strategy doesn't exist");
+
+        address previousStrategy = strategyIndexToAddress[_strategyId];
+        strategyAddressToIndex[previousStrategy] = 0;
+        strategyAddressToIndex[_strategy] = _strategyId;
+        strategyIndexToAddress[_strategyId] = _strategy;
+
+        emit StrategyUpdated(previousStrategy, _strategy, _strategyId);
     }
 }
