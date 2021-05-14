76017b8fbc999ee5c09c27ac9ba36201e2f43235
Michael Zhou <zhoumotongxue008@gmail.com>
2021-05-14 18:57:50 +0800

    Add StrategyAaveV2 deployment scripts and tests

--- test-strategy/StrategyAaveV2BUSD.spec.ts
+++ test-strategy/StrategyAaveV2BUSD.spec.ts
@@ -5,16 +5,16 @@ import { testStrategyAaveLendingPoolV2 } from './StrategyAaveLendingPoolV2.spec'
 
 dotenv.config();
 
-describe('StrategyAaveV2USDC', function () {
+describe('StrategyAaveV2BUSD', function () {
   it(DESCRIPTION, async function () {
     await testStrategyAaveLendingPoolV2(
       this,
-      process.env.STRATEGY_AAVE_V2_USDC,
-      'USDC',
-      6,
-      process.env.USDC as string,
-      process.env.AAVE_AUSDC as string,
-      process.env.USDC_FUNDER as string
+      process.env.STRATEGY_AAVE_V2_BUSD,
+      'BUSD',
+      18,
+      process.env.BUSD as string,
+      process.env.AAVE_ABUSD as string,
+      process.env.BUSD_FUNDER as string
     );
   });
 });
--- test-strategy/StrategyAaveV2SUSD.spec.ts
+++ test-strategy/StrategyAaveV2SUSD.spec.ts
@@ -5,16 +5,16 @@ import { testStrategyAaveLendingPoolV2 } from './StrategyAaveLendingPoolV2.spec'
 
 dotenv.config();
 
-describe('StrategyAaveV2USDC', function () {
+describe('StrategyAaveV2SUSD', function () {
   it(DESCRIPTION, async function () {
     await testStrategyAaveLendingPoolV2(
       this,
-      process.env.STRATEGY_AAVE_V2_USDC,
-      'USDC',
-      6,
-      process.env.USDC as string,
-      process.env.AAVE_AUSDC as string,
-      process.env.USDC_FUNDER as string
+      process.env.STRATEGY_AAVE_V2_SUSD,
+      'SUSD',
+      18,
+      process.env.SUSD as string,
+      process.env.AAVE_ASUSD as string,
+      process.env.SUSD_FUNDER as string
     );
   });
 });
